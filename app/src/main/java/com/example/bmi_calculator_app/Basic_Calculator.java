15cc92192df21b6b55e1cf788a3c43565c7c7027
Sheffalee <87572002+sheffalee@users.noreply.github.com>
2024-02-07 13:46:53 +0530

    all operations calculator updated

--- app/src/main/java/com/example/bmi_calculator_app/Basic_Calculator.java
+++ app/src/main/java/com/example/bmi_calculator_app/Basic_Calculator.java
@@ -4,11 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Basic_Calculator_Operations extends AppCompatActivity {
+
+public class Basic_Calculator extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_basic_calculator_operations);
+        setContentView(R.layout.activity_basic_calculator);
     }
 }
